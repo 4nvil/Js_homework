0f0d337c36be325362452b4e460a45affc919ac8
Ivan K. <xide@gmail.com>
2024-03-31 10:35:00 +0300

    HW 6.js added

---
+++ HW 6.js
@@ -0,0 +1,11 @@
+function Sum() {
+    let sum = 0;
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 !== 0) {
+            sum += i;
+        }
+    }
+    console.log("Сума чисел від 1 до 100 включно, які не кратні 3:", sum);
+}
+
+Sum();
